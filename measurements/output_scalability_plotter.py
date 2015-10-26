c98d6fcda339ff89e04b0be0f2ca825eb595fdf8
Philipp Weidel <p.weidel@fz-juelich.de>
2015-10-26 11:47:34 +0100

    created files

---
+++ measurements/output_scalability_plotter.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import json
+import pandas as pd
+import numpy as np
+
+import seaborn as sbn
+sbn.set_palette("deep", desat=.6)
+sbn.set_context(rc={"figure.figsize": (8, 4)})
+palette = sbn.color_palette()
+
+data_file = open("input_scalability.dat", 'r')
+
+data = pd.DataFrame(json.load(data_file))
+
+sbn.pointplot(x="num_neurons", y="run_time", data=data, color=palette[0])
+sbn.pointplot(x="num_neurons", y="build_time", data=data, color=palette[1])
+sbn.pointplot(x="num_neurons", y="real-time_factor", data=data, color=palette[2])
+plt.show()
+
