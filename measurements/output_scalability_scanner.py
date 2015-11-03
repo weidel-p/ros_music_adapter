3543ce7ccba121202a5a0e41837b8b7b34d8d8a6
Philipp Weidel <p.weidel@fz-juelich.de>
2015-11-03 16:12:49 +0100

    merged master and measurements/master

--- measurements/output_scalability_scanner.py
+++ measurements/output_scalability_scanner.py
@@ -9,24 +9,25 @@ MIN_NUM_NEURONS = 1
 MAX_NUM_NEURONS = 5002
 STEP_SIZE = 5000
 
-run_time = 10 # in sec
-run_time_build = 0.1 # in sec
+sim_time = 10 # in sec
+sim_time_build = 0.001 # in sec
 
-data_filename = "output_scalability.dat"
+data_filename = sys.argv[1]
 
 if os.path.exists(data_filename):
     os.remove(data_filename)
 
 
-data ={"num_neurons": [], "build_time": [], "run_time": [], "real-time_factor": []}
+data ={"num_neurons": [], "time": [], "type": [], "iteration": []}
 
 for num_neurons in np.arange(MIN_NUM_NEURONS, MAX_NUM_NEURONS, STEP_SIZE):
+    print "\n\n\n\n\ RUNNING", num_neurons, "NEURONS \n\n\n\n"
     music_base_config = \
-                "music_timestep=0.001\n\
-                [sensor]\n\
+                "[sensor]\n\
                   binary=../ros_sensor_adapter\n\
                   args=\n\
                   np=1\n\
+                  music_timestep=0.03333\n\
                   ros_topic=/jubot/laserscan\n\
                   message_type=Laserscan\n\
                   sensor_update_rate=30\n\
@@ -34,15 +35,18 @@ for num_neurons in np.arange(MIN_NUM_NEURONS, MAX_NUM_NEURONS, STEP_SIZE):
                   binary=../nef_encoder\n\
                   args=\n\
                   np=1\n\
+                  music_timestep=0.03333\n\
                 [decoder]\n\
                   binary=../linear_readout_decoder\n\
                   args=\n\
                   np=1\n\
+                  music_timestep=0.05\n\
                   tau=0.03\n\
                 [command]\n\
                   binary=../ros_command_adapter\n\
                   args=\n\
                   np=1\n\
+                  music_timestep=0.05\n\
                   ros_topic=/jubot/cmd_vel\n\
                   message_type=Twist\n\
                   linear.x=0\n\
@@ -52,40 +56,53 @@ for num_neurons in np.arange(MIN_NUM_NEURONS, MAX_NUM_NEURONS, STEP_SIZE):
                 encoder.out->decoder.in[" + str(num_neurons) +"]\n\
                 decoder.out->command.in[2]"
 
-    music_config_build = "stoptime=" + str(run_time_build) + "\n"\
+    music_config_build = "stoptime=" + str(sim_time_build) + "\n"\
                          + music_base_config
 
     music_config_build_file = open("config_build.music", 'w+')
     music_config_build_file.writelines(music_config_build)
     music_config_build_file.close()
 
-    music_config_run = "stoptime=" + str(run_time) + "\n"\
+    music_config_run = "stoptime=" + str(sim_time) + "\n"\
                        + music_base_config
 
     music_config_run_file = open("config_run.music", 'w+')
     music_config_run_file.writelines(music_config_run)
     music_config_run_file.close()
 
+    for it in range(ITERATIONS):
 
-    for _ in range(ITERATIONS):
         start = datetime.datetime.now()
         os.system("mpirun \-np 4 music config_build.music ")
         end = datetime.datetime.now()
 
         dt_build = end - start
-        data["build_time"].append((dt_build.seconds * 1000000 + dt_build.microseconds) / 1000000.)
+        build_time = dt_build.seconds + dt_build.microseconds / 1000000.
+
+        data["num_neurons"].append(num_neurons)
+        data['type'].append("build time")
+        data['iteration'].append(it)
+        data["time"].append(build_time)
 
         start = datetime.datetime.now()
         os.system("mpirun \-np 4 music config_run.music ")
         end = datetime.datetime.now()
         
         dt_run = end - start
-        data["run_time"].append((dt_run.seconds * 1000000 + dt_run.microseconds) / 1000000.)
+        run_time = dt_run.seconds + dt_run.microseconds / 1000000.
 
         data["num_neurons"].append(num_neurons)
+        data['type'].append("run time")
+        data['iteration'].append(it)
+        data["time"].append(run_time)
 
-        rtf = run_time / (data["run_time"][-1] - data["build_time"][-1])
-        data["real-time_factor"].append(rtf)
+        rtf = sim_time / (run_time - build_time)
+
+        data["num_neurons"].append(num_neurons)
+        data['type'].append("real-time factor")
+        data['iteration'].append(it)
+        data["time"].append(rtf)
+
     
     if os.path.exists("config_run.music"):
         os.remove("config_run.music")
