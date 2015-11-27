a6c162e6a10d921ca775351bf97200069f100fdb
Philipp Weidel <p.weidel@fz-juelich.de>
2015-11-27 15:05:04 +0100

    added num_neurons option

--- examples/braitenberg3/dummy_network.py
+++ examples/braitenberg3/dummy_network.py
@@ -10,6 +10,7 @@ to_ms = lambda t: t * 1000.
 opt_parser = OptionParser()
 opt_parser.add_option("-t", "--simtime", dest="simtime", type="float", help="Simulation time in s")
 opt_parser.add_option("-s", "--timestep", dest="music_timestep", type="float", help="MUSIC timestep")
+opt_parser.add_option("-n", "--num_neurons", dest="num_neurons", type="int", help="Number of encoding neurons")
 
 (options, args) = opt_parser.parse_args()
 
@@ -18,7 +19,7 @@ nest.set_verbosity("M_FATAL")
 nest.SetKernelStatus({'resolution': 1.0})
 #nest.SetKernelStatus({'print_time': True})
 
-NUM_ENC_NEURONS = 2 
+NUM_ENC_NEURONS = options.num_neurons 
 
 proxy_in = nest.Create('music_event_in_proxy', NUM_ENC_NEURONS)
 nest.SetStatus(proxy_in, [{'port_name': 'in', 'music_channel': c} for c in range(NUM_ENC_NEURONS)])
